24d64aa84f956cc9fd871ef618c2e0c5e0486d4e
Sean Pollock <seanpollock@Seans-MacBook-Air-2.local>
2020-11-02 17:44:09 -0500

    sign up with checks work on front end

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Signup from '../Components/Signup';
 import { Container } from 'react-bootstrap';
+import { AuthProvider } from '../Contexts/AuthContext';
 
 function App() {
   return (
-    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-      <div className="w-100" style={{ maxWidth: '400px' }}>
-        <Signup />
-      </div> 
-    </Container>
-   
+    <AuthProvider>
+      <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+        <div className="w-100" style={{ maxWidth: '400px' }}>
+          <Signup />
+        </div> 
+      </Container>
+    </AuthProvider>
   );
 }
 
